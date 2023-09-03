b2debe85259c8d457feaae9236c118a5909e7450
GreenPlanet0204 <greenplanet1724@gmail.com>
2023-09-04 06:04:37 +0900

    add coordinateinput

--- spacevision_frontend/src/components/searchplaceview/coordinateInput.js
+++ spacevision_frontend/src/components/searchplaceview/coordinateInput.js
@@ -1,196 +1,192 @@
 import React, { Fragment, useState, useEffect } from "react";
 
 import {
-	Text,
-	Flex,
-	NumberInput,
-	NumberInputField,
-	NumberInputStepper,
-	NumberIncrementStepper,
-	NumberDecrementStepper,
-	Slider,
-	Tooltip,
-	SliderTrack,
-	SliderFilledTrack,
-	SliderThumb,
-	SliderMark,
+  Text,
+  Flex,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  Slider,
+  Tooltip,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderMark,
 } from "@chakra-ui/react";
 
 import lodash from "lodash";
 
 const CoordinateInputView = (props) => {
-	const {
-		title,
-		inputFormat,
-		updatedValue = 0,
-		onChangeValue,
-		onChangeValueEnd,
-	} = props;
+  const {
+    title,
+    inputFormat,
+    updatedValue = 0,
+    onChangeValue,
+    onChangeValueEnd,
+  } = props;
 
-	const [state, setState] = useState({
-		value: updatedValue,
-		isOnChangeStart: false,
-	});
+  const [state, setState] = useState({
+    value: updatedValue,
+    isOnChangeStart: false,
+  });
 
-	const updateState = (data) =>
-		setState((preState) => ({ ...preState, ...data }));
+  const updateState = (data) =>
+    setState((preState) => ({ ...preState, ...data }));
 
-	const [showTooltip, setShowTooltip] = React.useState(false);
+  const [showTooltip, setShowTooltip] = React.useState(false);
 
-	/*  Life-cycles Methods */
+  /*  Life-cycles Methods */
 
-	useEffect(() => {
-		return () => {};
-	}, []);
+  useEffect(() => {
+    return () => {};
+  }, []);
 
-	useEffect(() => {
-		if (!lodash.isNil(updatedValue)) {
-			updateState({
-				value: updatedValue,
-			});
-		}
-	}, [updatedValue]);
+  useEffect(() => {
+    if (!lodash.isNil(updatedValue)) {
+      updateState({
+        value: updatedValue,
+      });
+    }
+  }, [updatedValue]);
 
-	/*  Public Interface Methods */
+  /*  Public Interface Methods */
 
-	/*  UI Events Methods   */
+  /*  UI Events Methods   */
 
-	const onChangeStart = () => {
-		updateState({
-			isOnChangeStart: true,
-		});
-	};
+  const onChangeStart = () => {
+    updateState({
+      isOnChangeStart: true,
+    });
+  };
 
-	const onMouseEnter = () => {
-		if (!(state?.isOnChangeStart ?? true)) {
-			setShowTooltip(true);
-		}
-	};
-	const onMouseLeave = () => {
-		if (!state?.isOnChangeStart ?? true) {
-			setShowTooltip(false);
-		}
-	};
+  const onMouseEnter = () => {
+    if (!(state?.isOnChangeStart ?? true)) {
+      setShowTooltip(true);
+    }
+  };
+  const onMouseLeave = () => {
+    if (!state?.isOnChangeStart ?? true) {
+      setShowTooltip(false);
+    }
+  };
 
-	const onChangeEnd = (value) => {
-		setShowTooltip(false);
+  const onChangeEnd = (value) => {
+    setShowTooltip(false);
 
-		updateState({
-			isOnChangeStart: false,
-		});
+    updateState({
+      isOnChangeStart: false,
+    });
 
-		onChangeValueEnd && onChangeValueEnd(value);
-	};
+    onChangeValueEnd && onChangeValueEnd(value);
+  };
 
-	const onChangeSliderValue = (value) => {
-		updateState({
-			value: value,
-		});
-		onChangeValue && onChangeValue(value);
-	};
+  const onChangeSliderValue = (value) => {
+    updateState({
+      value: value,
+    });
+    onChangeValue && onChangeValue(value);
+  };
 
-	/*  Server Request Methods  */
+  /*  Server Request Methods  */
 
-	/*  Server Response Methods  */
+  /*  Server Response Methods  */
 
-	/*  Server Response Handler Methods  */
+  /*  Server Response Handler Methods  */
 
-	/*  Custom-Component sub-render Methods */
+  /*  Custom-Component sub-render Methods */
 
-	const renderMasterContainer = () => {
-		const sliderMarkValueArray = [
-			inputFormat?.min,
-			inputFormat?.min + ((inputFormat?.max - inputFormat?.min) * 1) / 4,
-			(inputFormat?.min + inputFormat?.max) / 2,
-			(inputFormat?.max - inputFormat?.min) / 4,
-			inputFormat?.max,
-		];
+  const renderMasterContainer = () => {
+    const sliderMarkValueArray = [
+      inputFormat?.min,
+      inputFormat?.min + ((inputFormat?.max - inputFormat?.min) * 1) / 4,
+      (inputFormat?.min + inputFormat?.max) / 2,
+      (inputFormat?.max - inputFormat?.min) / 4,
+      inputFormat?.max,
+    ];
 
-		return (
-			<>
-				<Flex
-					flexDirection={"row"}
-					alignItems={"center"}
-					justifyContent={"center"}
-					me={2}
-					mt={2}
-				>
-					<Text
-						me={5}
-						fontSize={"sm"}
-						fontWeight={"medium"}
-					>
-						{`${title} :`}
-					</Text>
-					<Flex
-						flex={1}
-						flexDirection={"column"}
-						alignItems={"flex-start"}
-						justifyContent={"flex-start"}
-					>
-						<Slider
-							// aria-label='slider-ex-2'
-							focusThumbOnChange={false}
-							onChange={onChangeSliderValue}
-							onChangeStart={onChangeStart}
-							onMouseEnter={onMouseEnter}
-							onMouseLeave={onMouseLeave}
-							onChangeEnd={onChangeEnd}
-							mb={4}
-							value={state?.value}
-							{...inputFormat}
-							step={0.00001}
-						>
-							{sliderMarkValueArray.map((item, index) => {
-								return (
-									<SliderMark
-										key={`${index}`}
-										value={item}
-										mt="2"
-										ml="-2.5"
-										fontSize="x-small"
-									>
-										{`${item}`}
-									</SliderMark>
-								);
-							})}
-							<SliderTrack>
-								<SliderFilledTrack />
-							</SliderTrack>
-							<Tooltip
-								isOpen={showTooltip}
-								hasArrow
-								placement="top"
-								label={`${state?.value}`}
-							>
-								<SliderThumb bg={"blue.300"} />
-							</Tooltip>
-						</Slider>
-						<NumberInput
-							onChange={(value) => {
-								onChangeSliderValue(value);
-								onChangeValueEnd && onChangeValueEnd(value);
-							}}
-							value={state?.value}
-							mt={5}
-							size={"sm"}
-							allowMouseWheel={true}
-							{...inputFormat}
-							step={1}
-						>
-							<NumberInputField />
-							<NumberInputStepper>
-								<NumberIncrementStepper />
-								<NumberDecrementStepper />
-							</NumberInputStepper>
-						</NumberInput>
-					</Flex>
-				</Flex>
-			</>
-		);
-	};
+    return (
+      <>
+        <Flex
+          flexDirection={"row"}
+          alignItems={"center"}
+          justifyContent={"center"}
+          me={2}
+          mt={2}
+        >
+          <Text me={5} fontSize={"sm"} fontWeight={"medium"}>
+            {`${title} :`}
+          </Text>
+          <Flex
+            flex={1}
+            flexDirection={"column"}
+            alignItems={"flex-start"}
+            justifyContent={"flex-start"}
+          >
+            <Slider
+              // aria-label='slider-ex-2'
+              focusThumbOnChange={false}
+              onChange={onChangeSliderValue}
+              onChangeStart={onChangeStart}
+              onMouseEnter={onMouseEnter}
+              onMouseLeave={onMouseLeave}
+              onChangeEnd={onChangeEnd}
+              mb={4}
+              value={state?.value}
+              {...inputFormat}
+              step={0.00001}
+            >
+              {sliderMarkValueArray.map((item, index) => {
+                return (
+                  <SliderMark
+                    key={`${index}`}
+                    value={item}
+                    mt="2"
+                    ml="-2.5"
+                    fontSize="x-small"
+                  >
+                    {`${item}`}
+                  </SliderMark>
+                );
+              })}
+              <SliderTrack>
+                <SliderFilledTrack />
+              </SliderTrack>
+              <Tooltip
+                isOpen={showTooltip}
+                hasArrow
+                placement="top"
+                label={`${state?.value}`}
+              >
+                <SliderThumb bg={"blue.300"} />
+              </Tooltip>
+            </Slider>
+            <NumberInput
+              onChange={(value) => {
+                onChangeSliderValue(value);
+                onChangeValueEnd && onChangeValueEnd(value);
+              }}
+              value={state?.value}
+              mt={5}
+              size={"sm"}
+              allowMouseWheel={true}
+              {...inputFormat}
+              step={1}
+            >
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+          </Flex>
+        </Flex>
+      </>
+    );
+  };
 
-	return renderMasterContainer();
+  return renderMasterContainer();
 };
 
 export default CoordinateInputView;
