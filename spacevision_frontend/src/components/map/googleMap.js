f2e18df9f52f7bb86e1bbc965d7b8b50dce7bdfc
GreenPlanet0204 <greenplanet1724@gmail.com>
2023-09-04 06:02:25 +0900

    add googlemap

--- spacevision_frontend/src/components/map/googleMap.js
+++ spacevision_frontend/src/components/map/googleMap.js
@@ -1,28 +1,28 @@
-import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api'
-import { useEffect, useMemo, useRef, useState } from 'react'
-import { connect } from 'react-redux'
-import Actions from '../../redux/action'
-import './map.css'
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+import { useMemo, useState } from "react";
+import { connect } from "react-redux";
+import Actions from "../../redux/action";
+import "./map.css";
 
-const libraries = ['places']
+const libraries = ["places"];
 
 const GoogleApp = (props) => {
-  const { userPref } = props
-  const [latPosiont, setLatPosition] = useState(0)
-  const [lngPosition, setLngPosiont] = useState(0)
+  const { userPref } = props;
+  const [latPosiont, setLatPosition] = useState(0);
+  const [lngPosition, setLngPosiont] = useState(0);
 
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: 'AIzaSyCQBkQpI4jlGBQ4jCq_wE5mANL6InyztBc',
+    googleMapsApiKey: "AIzaSyCQBkQpI4jlGBQ4jCq_wE5mANL6InyztBc",
     libraries,
-  })
+  });
 
   const center = useMemo(
     () => ({
       lat: Number(userPref?.coordinate?.latitude),
       lng: Number(userPref?.coordinate?.longitude),
     }),
-    [],
-  )
+    []
+  );
 
   return (
     <>
@@ -35,28 +35,28 @@ const GoogleApp = (props) => {
           center={center}
           zoom={5}
           onClick={(e) => {
-            console.log(e.latLng.lat(), e.latLng.lng())
-            console.log('bounds', e)
-            setLatPosition(e.latLng.lat())
-            setLngPosiont(e.latLng.lng())
-            console.log('lat', latPosiont)
-            console.log('lng', lngPosition)
+            console.log(e.latLng.lat(), e.latLng.lng());
+            console.log("bounds", e);
+            setLatPosition(e.latLng.lat());
+            setLngPosiont(e.latLng.lng());
+            console.log("lat", latPosiont);
+            console.log("lng", lngPosition);
           }}
         ></GoogleMap>
       )}
     </>
-  )
-}
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
     userPref: state.userPref,
-  }
-}
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
     setUserPref: (userPref) => dispatch(Actions.setUserPref(userPref)),
-  }
-}
-export default connect(mapStateToProps, mapDispatchToProps)(GoogleApp)
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(GoogleApp);
